75e8e707f2106b2a808781446e203364d8e35579
1jiwoo27 <chloey1227@gmail.com>
2026-01-20 17:01:59 +0900

    chore: 클로드 코드 리뷰 반영 (eslint.config.js)

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -4,27 +4,31 @@ import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import simpleImportSort from 'eslint-plugin-simple-import-sort';
 import tseslint from 'typescript-eslint';
-import prettier from 'eslint-config-prettier';
-import { defineConfig, globalIgnores } from 'eslint/config';
+import prettier from 'eslint-config-prettier/flat';
+import { defineConfig } from 'eslint/config';
 
 export default defineConfig([
-  globalIgnores(['dist']),
-
+  {
+    ignores: ['dist'],
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
   {
     files: ['**/*.{ts,tsx}'],
-    plugins: {
-      'simple-import-sort': simpleImportSort,
-    },
-    extends: [
-      js.configs.recommended,
-      ...tseslint.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-      prettier,
-    ],
+    ...reactHooks.configs.flat.recommended,
+  },
+  {
+    files: ['**/*.{ts,tsx}'],
+    ...reactRefresh.configs.vite,
+  },
+  {
+    files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+    },
+    plugins: {
+      'simple-import-sort': simpleImportSort,
     },
     rules: {
       'simple-import-sort/imports': [
@@ -42,4 +46,5 @@ export default defineConfig([
       'simple-import-sort/exports': 'error',
     },
   },
+  prettier,
 ]);
