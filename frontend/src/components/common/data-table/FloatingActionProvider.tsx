25e96057bfa709fc5bde0b2256467cad43d15012
Mingyu Seong <minngyuseong@gmail.com>
2026-01-26 16:34:21 +0900

    style:FloatingActionProvider 색 반영

--- frontend/src/components/common/data-table/FloatingActionProvider.tsx
+++ frontend/src/components/common/data-table/FloatingActionProvider.tsx
@@ -1,4 +1,9 @@
+import { Icons } from '@/assets';
+
 import { useDataTable } from './context';
+
+const Divider = () => <div className="bg-line-solid-normal/30 h-6 w-px" />;
+const Spacer = () => <div className="w-18.5" />;
 
 const FloatingActionProvider = () => {
   const { table, tableState, dispatch } = useDataTable();
@@ -7,10 +12,12 @@ const FloatingActionProvider = () => {
   if (tableState.floatingBarVariant === 'NONE') return null;
 
   return (
-    <div className="fixed bottom-10 left-1/2 -translate-x-1/2 rounded-lg border bg-white p-4 shadow-2xl">
+    <div className="bg-inverse-background text-inverse-label body2-normal-bold fixed bottom-30 left-1/2 -translate-x-1/2 truncate rounded-xl p-3 px-3.5 shadow-2xl">
       {tableState.floatingBarVariant === 'MANAGEMENT' ? (
         // 1번 바: 카테고리/결제수단/여행/숨김
-        <div className="flex gap-4">
+        <div className="flex items-center gap-4">
+          <span>{selectedRows.length}개 선택됨</span>
+          <Spacer />
           <button
             onClick={() => {
               /* 카테고리 변경 로직 */
@@ -18,6 +25,7 @@ const FloatingActionProvider = () => {
           >
             카테고리
           </button>
+          <Divider />
           <button
             onClick={() => {
               /* 결제수단 변경 로직 */
@@ -25,7 +33,18 @@ const FloatingActionProvider = () => {
           >
             결제수단
           </button>
-          <button>숨김</button>
+          <Divider />
+          <button
+            onClick={() => {
+              /* 결제수단 변경 로직 */
+            }}
+          >
+            여행
+          </button>
+          <Divider />
+          <button className="text-status-negative">삭제</button>
+          <Divider />
+          <Icons.Close height={10} width={10} />
         </div>
       ) : (
         // 2번 바: 현재 가계부에 추가하기
