824cbefd9b8e15520847590fc7b724d0a7e51f14
1jiwoo27 <chloey1227@gmail.com>
2026-01-27 15:09:21 +0900

    feat: dropdown 컴포넌트 구현 (filter 컴포넌트 함께 사용)

---
+++ frontend/src/components/common/dropdown/Dropdown.tsx
@@ -0,0 +1,50 @@
+import { useState } from 'react';
+
+import Filter from '../Filter';
+
+import OptionItem from './OptionItem';
+
+interface Option {
+  id: string;
+  name: string;
+}
+
+interface DropDownProps {
+  selected: string | null;
+  onSelect: (value: string) => void;
+  options?: Option[];
+}
+
+const DEFAULT_PLACEHOLDER = '미국 교환학생';
+
+const DropDown = ({ selected, onSelect, options }: DropDownProps) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleOptionClick = (value: string) => {
+    onSelect(value);
+    setIsOpen(false);
+  };
+
+  return (
+    <div className="relative inline-block w-fit">
+      <Filter isOpen={isOpen} onClick={() => setIsOpen(v => !v)}>
+        {selected || DEFAULT_PLACEHOLDER}
+      </Filter>
+
+      {isOpen && (
+        <ul className="absolute left-0 top-full z-50 mt-2 w-48 overflow-hidden rounded-md border border-line-normal-normal bg-background-normal shadow-lg">
+          {options?.map((option) => (
+            <OptionItem
+              key={option.id}
+              label={option.name}
+              isSelected={option.name === selected}
+              onSelect={handleOptionClick}
+            />
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default DropDown;
