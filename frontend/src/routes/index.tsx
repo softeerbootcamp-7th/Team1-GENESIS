2964de6759338f2e11b63c5cd0889ae07d2aa64b
Mingyu Seong <112471084+minngyuseong@users.noreply.github.com>
2026-01-29 16:38:38 +0900

    [FE] [FEAT] 커스텀 fetch 구현 (#143)
    * feat: tanstack auth 반영
    - 세션 검증 실패 시 로그인 화면으로 리다이렉트
    - 세션 검증 성공 시 전역 상태에 온보딩 해야하는지 여부 저장
    
    * feat: 인증 여부에 따라 index, home으로 초기 라우팅하기
    - zustand로 간단 인증 상태 저장함
    - 추후 oidc 라이브러리로 바꿀 예정
    
    * feat: oidc 라이브버리 설치
    
    * chore: oidc 패키지 제거
    - SPA방식이 아닌 BFF방식으로 로그인 관리하기 위해서 기존 라이브러리 제거
    
    * fix: 전역 auth 상태 제거
    - user 정보를 queryClient에 저장
    
    * feat: 커스텀 fetch 구현
    - Error를 상속받으며 추가로 status(number)를 필드로 가지는 커스텀 에러 타입 ApiError 구현
    - customFetch에서 HTTP 에러 핸들링을 ApiError를 생성하여 던짐으로써 처리
    - HTTP 상태 코드 및 에러 종류를 상수로 관리
    
    * feat: router전역에서 사용할 queryClient 구현
    - HTTP 응답으로 401(Unauthorized) 받으면 login으로 redirect하도록 전역에서 ApiError 핸들링
    
    * feat: pendingComponent, errorComponent구현
    - shadcn Skeleton으로 pending구현
    - ErrorFallback으로 error구현
    
    * chore: 임시 auth 관련 api구현
    - 모든 페이지를 돌아다닐 떄 호출되지 않고,  staleTime을 10분으로 설정해 무효화되면 호출되로고 함
    - 추후 엔드포인트 정해지면 수정될 예정
    
    * chore: pnpm-lock 업데이트
    
    * chore: tanstack 캐싱 폴더 제거 및 추적 중단
    
    * chore: BASE_URL 환경변수 파일에서 관리
    
    * chore: root에서 에러핸들릥
    
    * chore: 로그인 시 리다이렉트 구현 및 커스텀 Error 사용하도록 코드 수정
    
    * chore: 주석 제거
    
    * chore: 주석 제거
    
    * refactor: auth 체크 로직 함수화
    
    * chore: pnpm-lock 린트 적용

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,6 +1,15 @@
 import { createFileRoute } from '@tanstack/react-router';
 
+import { Skeleton } from '@/components/ui/skeleton';
+
+import { redirectIfAuthenticated } from '@/lib/auth';
+
 export const Route = createFileRoute('/')({
+  beforeLoad: async () => {
+    const user = await redirectIfAuthenticated();
+    return { user };
+  },
+  pendingComponent: () => <Skeleton className="h-64" />,
   component: RouteComponent,
 });
 
