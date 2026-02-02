0b3cb88577749b3bdf14b944df84c2124532200b
Mingyu Seong <minngyuseong@gmail.com>
2026-02-02 15:55:53 +0900

    chore: 임시 코드 제거

--- frontend/src/routes/_app/home.tsx
+++ frontend/src/routes/_app/home.tsx
@@ -1,15 +1,9 @@
 import { createFileRoute } from '@tanstack/react-router';
 
-import CurrencyConverter from '@/components/common/side-panel/CurrencyConverter';
-
 export const Route = createFileRoute('/_app/home')({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  return (
-    <div className="p-4">
-      <CurrencyConverter />
-    </div>
-  );
+  return <div>Hello "/home"!</div>;
 }
