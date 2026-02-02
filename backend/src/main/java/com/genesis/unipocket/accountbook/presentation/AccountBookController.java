d54e7c028bd5bb0bb7c54d0fea9fcce1ab4e2f79
blu3fishez <kcw2205@naver.com>
2026-02-02 11:59:02 +0900

    feat(account_book): 가계부 생성 Controller 스켈레톤 코드 추가

---
+++ backend/src/main/java/com/genesis/unipocket/accountbook/presentation/AccountBookController.java
@@ -0,0 +1,26 @@
+package com.genesis.unipocket.accountbook.presentation;
+
+import com.genesis.unipocket.accountbook.facade.AccountBookFacade;
+import com.genesis.unipocket.accountbook.presentation.dto.request.CreateAccountBookReq;
+import com.genesis.unipocket.accountbook.presentation.dto.response.CreateAccountBookRes;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class AccountBookController {
+
+	private final AccountBookFacade accountBookFacade;
+
+	@PostMapping("/account-books")
+	public ResponseEntity<CreateAccountBookRes> createAccountBook(
+			@RequestBody CreateAccountBookReq req) {
+
+		return ResponseEntity.ok(CreateAccountBookRes.builder().build());
+	}
+}
