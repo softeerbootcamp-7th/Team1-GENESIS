032abb8c1716bde299d681d56e3258da11bbe29f
1jiwoo27 <chloey1227@gmail.com>
2026-02-02 15:52:31 +0900

    feat: useDebouncedEffect hook 생성 (자동 저장용)

---
+++ frontend/src/hooks/useDebouncedEffect.tsx
@@ -0,0 +1,23 @@
+import { useEffect, useRef } from 'react';
+
+export const useDebouncedEffect = (
+  effect: () => void | (() => void),
+  deps: React.DependencyList,
+  delay = 500,
+) => {
+  const cleanupRef = useRef<ReturnType<typeof effect> | undefined>(undefined);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      cleanupRef.current = effect();
+    }, delay);
+
+    return () => {
+      clearTimeout(timer);
+      if (typeof cleanupRef.current === 'function') {
+        cleanupRef.current();
+      }
+    };
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [...deps, delay]);
+};
