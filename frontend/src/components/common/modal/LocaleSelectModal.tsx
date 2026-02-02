911349e0be8f77ea2c4022b0bf38b9196d6ae7ca
Kjiw0n <128016888+Kjiw0n@users.noreply.github.com>
2026-02-02 11:21:31 +0900

    [FE] [FEAT] 온보딩 페이지 UI 구현 (#172)
    * chore: flags 이미지 src/ -> public/ 으로 이전
    
    - 국가 이미지는 바뀌는 파일이 아니고, 국가를 나타내는 데이터 이미지이므로 public으로 이동(빌드 과정에서 제외)
    
    * chore: 국가코드{imageUrl, countryName, currencySign, currencyName, currencyNameKor} json 데이터 및 국가 코드 리스트 저장
    
    * style: 서비스 화면 크기 벗어나는 스크롤 방지
    
    * chore: countryCode array 타입화
    
    * feat: LocaleSelectModal 컴포넌트 구현
    
    - control(radio) 컴포넌트 미적용
    
    - 기준통화, 현지통화에 따라 텍스트 변경
    
    - 현재 경로에 따라 텍스트 변경
    
    * feat: Control 컴포넌트 제작 (radio button)
    
    * feat: Control 컴포넌트 연동
    
    * feat: getCountryInfo 유틸 구현 (국가code 기반으로 CountryInfo(코드, imageUrl, 국가명, 통화기호, 통화면) 리턴)
    
    * refactor: getCountryInfo 적용 (LocaleSelectModal, LocaleConfirmModal)
    
    * style: height 및 scroll 조정
    
    * feat: 온보딩페이지(InitPage) UI 구현
    
    - /init 페이지에서 Menu, Header의 일부 UI만 제공하도록 적용
    
    * chore: 사용하지 않는 파일 제거
    
    * style: bg 색상 지정
    
    * chore: flagImage 경로 수정(public/ -> /)
    
    * chore: CountryInfo 인터페이스에 currencyNameKor: string 추가
    
    * chore: country_data -> countryData
    
    * chore: move InitPage.tsx (components/ -> pages)
    
    * style: input 임시 스타일 적용 및 font 적용
    
    * style: 마지막 Item의 border-bottom 제거
    
    * feat: CountryItem 전체 클릭 시 해당 Item select

---
+++ frontend/src/components/common/modal/LocaleSelectModal.tsx
@@ -0,0 +1,122 @@
+import { useState } from 'react';
+import { useMatches } from '@tanstack/react-router';
+import clsx from 'clsx';
+
+import Control from '@/components/common/Control';
+
+import { countryCode } from '@/data/countryCode';
+import { getCountryInfo } from '@/lib/country';
+
+interface CountryItemProps {
+  flagImg: string;
+  country: string;
+  currency: string;
+  checked: boolean;
+  value: string;
+  onChange: (value: string) => void;
+  isLast: boolean;
+}
+
+const CountryItem = ({
+  flagImg,
+  country,
+  currency,
+  checked,
+  value,
+  onChange,
+  isLast,
+}: CountryItemProps) => {
+  return (
+    <div
+      className={clsx(
+        'border-line-normal-normal flex w-full cursor-pointer items-center justify-center gap-6 border-b py-5 pr-2.5 pl-2',
+        isLast && 'border-b-0',
+      )}
+      onClick={() => onChange(value)}
+    >
+      <Control
+        name="currency-select"
+        value={value}
+        checked={checked}
+        onChange={onChange}
+      />
+      <div className="flex w-full justify-between">
+        <div className="flex gap-4">
+          <img src={flagImg} alt={`${country} flag`} />
+          <span>{country}</span>
+        </div>
+        <span>{currency}</span>
+      </div>
+    </div>
+  );
+};
+
+type LocaleSelectMode = 'BASE' | 'LOCAL'; // 기준 통화, 현지 통화
+
+interface LocaleSelectModalProps {
+  mode: LocaleSelectMode;
+}
+
+const LocaleSelectModal = ({ mode }: LocaleSelectModalProps) => {
+  const matches = useMatches();
+  const isInitPath = matches.some((match) => match.routeId === '/_app/init');
+  const [selectedCode, setSelectedCode] = useState<string>('');
+
+  const getTextContent = () => {
+    if (mode === 'BASE') {
+      return {
+        title: '기준 통화 변경',
+        subTitle: '선택된 통화로 가계부의 통합 계산 기준이 됩니다',
+      };
+    }
+
+    // LOCAL mode
+    return {
+      title: isInitPath ? '교환학생 가는 나라는 어디신가요?' : '국가/통화 변경',
+      subTitle: '선택된 나라를 기준으로 현지통화가 설정됩니다',
+    };
+  };
+
+  const { title, subTitle } = getTextContent();
+  return (
+    <div className="rounded-modal-20 bg-background-normal flex flex-col items-center gap-12 px-7.5 py-13">
+      {/* text section */}
+      <div className="flex flex-col items-center gap-1.5 text-center">
+        <span className="text-label-normal title3-semibold">{title}</span>
+        <span className="text-label-alternative heading2-medium">
+          {subTitle}
+        </span>
+      </div>
+
+      {/* select section */}
+      <div className="flex flex-col gap-4.5">
+        <input
+          type="text"
+          className="bg-fill-normal rounded-modal-10 h-15 w-full"
+        />
+        <div className="flex h-142.5 w-118 flex-col overflow-y-auto">
+          {countryCode.map((code, index) => {
+            const data = getCountryInfo(code);
+
+            if (!data) return null;
+
+            return (
+              <CountryItem
+                key={code}
+                value={code}
+                flagImg={data.imageUrl}
+                country={data.countryName}
+                currency={`${data.currencySign} ${data.currencyName}`}
+                checked={selectedCode === code}
+                onChange={setSelectedCode}
+                isLast={index === countryCode.length - 1}
+              />
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LocaleSelectModal;
