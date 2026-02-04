dfd8b6cfd17a030afdac7bb948cb28db619746e9
1jiwoo27 <chloey1227@gmail.com>
2026-02-04 16:44:51 +0900

    feat: toast 컴포넌트 퍼블리싱

--- frontend/src/components/common/Toast.tsx
+++ frontend/src/components/common/Toast.tsx
@@ -1,17 +1,24 @@
-import Icons from '@/assets';
+import { Icons } from '@/assets';
 
 interface ToastProps {
-    type: 'success' | 'error';
-    message: string;
+  type: 'success' | 'error';
+  message: string;
 }
 
+const ICON_MAP = {
+  success: Icons.CheckmarkCircle,
+  error: Icons.AlertCircle,
+} as const;
 
-const Toast = ({ type, message }: ToastProps) => {
+const Toast = ({ type, message = '텍스트' }: ToastProps) => {
+  const Icon = ICON_MAP[type];
+
   return (
-    <div className="w-105">
-        {message}
+    <div className="bg-inverse-background/50 bg-backdrop-blur-32 flex w-105 gap-3 rounded-xl px-4 py-2.75">
+      <Icon className="h-5.5 w-5.5" />
+      <span className="body2-normal-bold text-inverse-label">{message}</span>
     </div>
-  )
-}
+  );
+};
 
-export default Toast
+export default Toast;
