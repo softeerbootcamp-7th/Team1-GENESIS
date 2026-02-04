6d0f7569041c932f999821c915d74ab73c12135c
codingbaraGo <hyunchang52@gmail.com>
2026-02-04 11:14:34 +0900

    feat(expense): Presentation 계층 개발 - Controller 클래스 개발 - DTO 정의

---
+++ backend/src/main/java/com/genesis/unipocket/expense/presentation/dto/ExpenseManualCreateRequest.java
@@ -0,0 +1,24 @@
+package com.genesis.unipocket.expense.presentation.dto;
+
+import com.genesis.unipocket.expense.common.enums.Category;
+import com.genesis.unipocket.global.common.enums.CurrencyCode;
+import jakarta.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+
+/**
+ * <b>지출내역 생성 - 수기 DTO</b>
+ * <p>Client -> Presentation
+ *
+ * @author codingbaraGo
+ * @since 2026-02-03
+ */
+public record ExpenseManualCreateRequest(
+		@NotNull String merchantName,
+		Category category,
+		String paymentMethod,
+		@NotNull LocalDateTime occurredAt,
+		@NotNull Double localAmount,
+		@NotNull CurrencyCode localCurrency,
+		@NotNull Double standardAmount,
+		@NotNull CurrencyCode standardCurrency,
+		String memo) {}
