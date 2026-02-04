99d246a176d3da7677c3b94511ec6df800acbd13
1jiwoo27 <chloey1227@gmail.com>
2026-02-04 16:46:47 +0900

    chore: 아이콘과 텍스트 세로 정렬

--- frontend/src/components/common/Toast.tsx
+++ frontend/src/components/common/Toast.tsx
@@ -14,7 +14,7 @@ const Toast = ({ type, message = '텍스트' }: ToastProps) => {
   const Icon = ICON_MAP[type];
 
   return (
-    <div className="bg-inverse-background/50 bg-backdrop-blur-32 flex w-105 gap-3 rounded-xl px-4 py-2.75">
+    <div className="bg-inverse-background/50 bg-backdrop-blur-32 flex w-105 gap-3 rounded-xl px-4 py-2.75 items-center">
       <Icon className="h-5.5 w-5.5" />
       <span className="body2-normal-bold text-inverse-label">{message}</span>
     </div>
