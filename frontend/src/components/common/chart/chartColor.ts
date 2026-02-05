e25579a1ac295358d4379d825abfe5346e29cea0
Kjiw0n <rwd0904@naver.com>
2026-02-06 00:21:19 +0900

    chore: 차트에 사용되는 컬러 정의

---
+++ frontend/src/components/common/chart/chartColor.ts
@@ -0,0 +1,9 @@
+export const CATEGORY_COLORS = [
+  'var(--color-teal-40)',
+  'var(--color-teal-50)',
+  'var(--color-teal-70)',
+  'var(--color-teal-80)',
+  'var(--color-teal-95)',
+  'var(--color-teal-97)',
+  'var(--color-fill-strong)',
+];
