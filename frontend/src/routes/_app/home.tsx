19f527bbc92e0bc139ec3a1200916edfe72664e9
1jiwoo27 <chloey1227@gmail.com>
2026-02-05 14:22:02 +0900

    chore: test 용 side-panel 추가

--- frontend/src/routes/_app/home.tsx
+++ frontend/src/routes/_app/home.tsx
@@ -1,9 +1,11 @@
 import { createFileRoute } from '@tanstack/react-router';
 
+import SidePanel from '@/components/common/side-panel/SidePanel';
+
 export const Route = createFileRoute('/_app/home')({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  return <div>Home</div>;
+  return <div><SidePanel /></div>;
 }
