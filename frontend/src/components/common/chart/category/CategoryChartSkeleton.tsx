2a88fda95359b87e5702b3b2f7661569af1e55f1
Kjiw0n <rwd0904@naver.com>
2026-02-06 00:33:32 +0900

    feat: 차트 스켈레톤 컴포넌트(CategoryPieChartSkeleton, CategoryListItemSkeleton) 구현

---
+++ frontend/src/components/common/chart/category/CategoryChartSkeleton.tsx
@@ -0,0 +1,31 @@
+import CategoryPieChart from './CategoryPieChart';
+import { dummyData } from './dummy';
+
+export const CategoryPieChartSkeleton = () => {
+  return (
+    <div>
+      <CategoryPieChart
+        data={dummyData.items.map((item) => ({ percentage: item.percent }))}
+        totalAmount={
+          <div className="bg-fill-normal rounded-modal-4 h-5.5 w-32.5" />
+        }
+        colors={['var(--color-fill-strong)']}
+      />
+    </div>
+  );
+};
+
+export const CategoryListItemSkeleton = () => {
+  return (
+    <div className="flex h-6 w-52 items-center gap-2.5">
+      <div className="bg-fill-strong size-3.5" />
+      <div className="flex w-full items-center justify-between">
+        <div className="flex items-center gap-1.5">
+          <div className="bg-fill-strong rounded-modal-4 h-3 w-5" />
+          <div className="bg-fill-normal rounded-modal-4 h-3 w-6.5" />
+        </div>
+        <div className="bg-fill-normal rounded-modal-4 h-3 w-25" />
+      </div>
+    </div>
+  );
+};
