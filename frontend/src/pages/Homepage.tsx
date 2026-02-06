7e9627e2168c8d4a165712f275325b20677a0c7c
Mingyu Seong <minngyuseong@gmail.com>
2026-02-06 18:19:35 +0900

    chore: 임시 DataTable 높이 미지정하기

--- frontend/src/pages/Homepage.tsx
+++ frontend/src/pages/Homepage.tsx
@@ -1,17 +1,17 @@
 import Button from '@/components/common/Button';
 import { DataTable } from '@/components/common/data-table/DataTable';
+import DataTableCellEditor from '@/components/common/data-table/DataTableCellEditor';
 import DataTableProvider from '@/components/common/data-table/DataTableProvider';
+import SelectionActionProvider from '@/components/common/data-table/SelectionActionProvider';
 import Divider from '@/components/common/Divider';
 import { columns } from '@/components/home-page/columns';
 import ExpenseCard from '@/components/home-page/ExpenseCard';
 import { type Expense, getData } from '@/components/landing-page/dummy';
 
-// import { Icons } from '@/assets';
-
 const Homepage = () => {
   const data = getData();
   return (
-    <div className="flex flex-col px-30 pt-8">
+    <div className="flex h-full flex-col overflow-y-auto px-30 pt-8">
       <div className="mb-10 flex items-end gap-4">
         <ExpenseCard
           label="총 지출"
@@ -33,10 +33,12 @@ const Homepage = () => {
           위젯 편집하기
         </Button>
       </div>
-      <div className="border-label-alternative mb-5 flex h-70 items-center justify-center rounded-lg border border-dashed">
-        Widget area
+      <div className="border-label-alternative mb-5 flex items-center justify-center rounded-lg border border-dashed">
+        <span className="flex h-70 items-center justify-center">
+          Widget area
+        </span>
       </div>
-      <div className="bg-background-normal relative rounded-2xl px-2 py-4 shadow">
+      <div className="bg-background-normal rounded-2xl px-2 py-4 shadow">
         {/* <Icons.ChevronBack className="text-label-alternative absolute left-1/2 z-50 size-12 -translate-x-1/2 rotate-90" /> */}
         <DataTableProvider columns={columns} data={data}>
           <DataTable
@@ -48,6 +50,8 @@ const Homepage = () => {
               })
             }
           />
+          <DataTableCellEditor />
+          <SelectionActionProvider />
         </DataTableProvider>
       </div>
     </div>
