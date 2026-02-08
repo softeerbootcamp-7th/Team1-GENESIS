8d6539331c617b9eaa37b25b8298e820986c5b3c
Mingyu <minngyuseong@gmail.com>
2026-02-09 00:06:47 +0900

    fix: 로그인 여부에 따른 redirect 로직 제거 - customFetch에서 401시 reissue 인터셉터 구현으로 해결 - _auth페이지는 로그인 세션있으면 /home으로 리다이렉트 시켜야하는데, 추후 다른 브랜치에서 로직 추가예정

--- frontend/src/routes/_app.tsx
+++ frontend/src/routes/_app.tsx
@@ -4,16 +4,7 @@ import Header from '@/components/common/Header';
 import Menu from '@/components/common/menu/Menu';
 import { Skeleton } from '@/components/ui/skeleton';
 
-import { requireAuth } from '@/lib/auth';
-
 export const Route = createFileRoute('/_app')({
-  // 🔒 로그인 체크 로직
-  beforeLoad: async ({ location }) => {
-    // API 응답이 성공했지만 유저 정보가 없는 경우에만 수동 리다이렉트 (lib/auth.ts로 위임)
-    const user = await requireAuth(location.href);
-
-    return { user };
-  },
   pendingComponent: () => <Skeleton className="h-64" />,
   component: AppLayout,
 });
--- frontend/src/routes/_auth.tsx
+++ frontend/src/routes/_auth.tsx
@@ -3,13 +3,7 @@ import { createFileRoute, Outlet } from '@tanstack/react-router';
 import LandingHeader from '@/components/landing-page/LandingHeader';
 import { Skeleton } from '@/components/ui/skeleton';
 
-import { redirectIfAuthenticated } from '@/lib/auth';
-
 export const Route = createFileRoute('/_auth')({
-  beforeLoad: async () => {
-    const user = await redirectIfAuthenticated();
-    return { user };
-  },
   pendingComponent: () => <Skeleton className="h-64" />,
   component: AppLayout,
 });
