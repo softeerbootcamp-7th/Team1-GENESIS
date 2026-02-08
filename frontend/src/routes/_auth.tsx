111700e68d9636c474462147a05b45e169dafc67
Mingyu <minngyuseong@gmail.com>
2026-02-09 00:53:52 +0900

    feat: /auth 경로 진입시 유효한 accessToken 존재시 home으로 리다이렉트

--- frontend/src/routes/_auth.tsx
+++ frontend/src/routes/_auth.tsx
@@ -1,9 +1,24 @@
-import { createFileRoute, Outlet } from '@tanstack/react-router';
+import { createFileRoute, Outlet, redirect } from '@tanstack/react-router';
 
 import LandingHeader from '@/components/landing-page/LandingHeader';
 import { Skeleton } from '@/components/ui/skeleton';
 
+import { getUser } from '@/api/user/api';
+
 export const Route = createFileRoute('/_auth')({
+  beforeLoad: async () => {
+    const redirectIfAuthenticated = async () => {
+      // useQuery 사용하지 않는 이유는 캐싱된 데이터를 사용하지 않고
+      // 항상 최신 인증 상태를 확인하기 위함
+      const user = await getUser();
+      if (user) {
+        throw redirect({
+          to: '/home',
+        });
+      }
+    };
+    await redirectIfAuthenticated();
+  },
   pendingComponent: () => <Skeleton className="h-64" />,
   component: AppLayout,
 });
