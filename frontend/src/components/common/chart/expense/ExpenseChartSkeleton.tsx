8647a58a1b446527214d85157434f59f13d28c3c
Kjiw0n <rwd0904@naver.com>
2026-02-08 18:25:08 +0900

    style: ExpenseChart 스켈레톤 컴포넌트 추가

---
+++ frontend/src/components/common/chart/expense/ExpenseChartSkeleton.tsx
@@ -0,0 +1,47 @@
+import VerticalBarChart from '@/components/common/chart/charts/VerticalBarChart';
+import { EXPENSE_SUSPENSE_COLOR } from '@/components/common/chart/chartType';
+import { mockDataForCurrency } from '@/components/common/chart/expense/mock';
+
+const ExpenseLegendItemSkeleton = () => {
+  return (
+    <div className="flex w-40 animate-pulse items-center justify-between">
+      <div className="caption2-medium text-label-neutral flex items-center gap-1.75">
+        <div
+          className="size-3"
+          style={{ backgroundColor: EXPENSE_SUSPENSE_COLOR[0] }}
+        />
+        <div
+          className="rounded-modal-4 h-3 w-8"
+          style={{ backgroundColor: EXPENSE_SUSPENSE_COLOR[0] }}
+        />
+        <div
+          className="rounded-modal-4 h-3 w-10"
+          style={{ backgroundColor: EXPENSE_SUSPENSE_COLOR[0] }}
+        />
+      </div>
+      <div
+        className="rounded-modal-4 h-3 w-6"
+        style={{ backgroundColor: EXPENSE_SUSPENSE_COLOR[1] }}
+      />
+    </div>
+  );
+};
+
+const ExpenseChartSkeleton = () => {
+  return (
+    <>
+      <VerticalBarChart
+        data={mockDataForCurrency}
+        gap={2}
+        colors={[EXPENSE_SUSPENSE_COLOR[0]]}
+      />
+      <div className="flex flex-col items-start justify-center gap-2.5">
+        {Array.from({ length: 5 }).map((_, idx) => (
+          <ExpenseLegendItemSkeleton key={idx} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default ExpenseChartSkeleton;
