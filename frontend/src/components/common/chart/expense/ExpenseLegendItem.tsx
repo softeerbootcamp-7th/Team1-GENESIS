3ee44031c41ecd8f6fdf96bc902403151870ca01
Kjiw0n <rwd0904@naver.com>
2026-02-08 18:24:42 +0900

    feat: ExpenseChart의 범례를 나타내는 아이템 추가(ExpenseLegendItem)

---
+++ frontend/src/components/common/chart/expense/ExpenseLegendItem.tsx
@@ -0,0 +1,28 @@
+interface ExpenseLegendItemProps {
+  label: string;
+  currencySignAndName?: string;
+  percent: number;
+  color: string;
+}
+
+const ExpenseLegendItem = ({
+  label,
+  currencySignAndName,
+  percent,
+  color,
+}: ExpenseLegendItemProps) => {
+  return (
+    <div className="flex w-40 items-center justify-between">
+      <div className="caption2-medium text-label-neutral flex items-center gap-1.75">
+        <div className="h-3.5 w-3.5" style={{ backgroundColor: color }} />
+        {currencySignAndName && <span>{currencySignAndName}</span>}
+        <span className="max-w-15 truncate">{label}</span>
+      </div>
+      <span className="figure-caption1-medium text-label-alternative">
+        {percent}%
+      </span>
+    </div>
+  );
+};
+
+export default ExpenseLegendItem;
