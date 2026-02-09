8700c300dcb7a0151aa26de3416a6959b3584e2a
1jiwoo27 <chloey1227@gmail.com>
2026-02-09 16:55:59 +0900

    feat: report 컴포넌트 레이아웃 설정

---
+++ frontend/src/components/report-page/category/ReportCategory.tsx
@@ -0,0 +1,11 @@
+import ReportContainer from "../layout/ReportContainer"
+
+const ReportCategory = () => {
+  return (
+    <ReportContainer title="ReportCategory">
+      ReportCategory
+    </ReportContainer>
+  )
+}
+
+export default ReportCategory
