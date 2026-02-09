ae2f1738ea50ce576b0c2f766198f4136866867b
1jiwoo27 <chloey1227@gmail.com>
2026-02-09 14:51:03 +0900

    feat: currency_options 사용 및 스켈레톤 연결

--- frontend/src/components/common/chart/comparison/ComparisonChart.tsx
+++ frontend/src/components/common/chart/comparison/ComparisonChart.tsx
@@ -1,33 +1,36 @@
 import { useState } from 'react';
 
+import { CURRENCY_OPTIONS } from '@/components/common/chart/chartType';
+import ComparisonChartSkeleton from '@/components/common/chart/comparison/ComparisonChartSkeleton';
 import ComparisonChartView from '@/components/common/chart/comparison/ComparisonChartView';
 import ChartContainer from '@/components/common/chart/layout/ChartContainer';
 import ChartContent from '@/components/common/chart/layout/ChartContent';
 import ChartHeader from '@/components/common/chart/layout/ChartHeader';
 import DropDown from '@/components/common/dropdown/Dropdown';
 
-const ComparisonChart = () => {
-  const [selectedId, setSelectedId] = useState<number>(1);
-
-  const options = [
-    { id: 1, name: '기준 통화' },
-    { id: 2, name: '현지 통화' },
-  ];
+const ComparisonChart = ({ isLoading = false }: { isLoading?: boolean }) => {
+  const [selectedCurrency, setSelectedCurrency] = useState<number>(
+    CURRENCY_OPTIONS[0].id,
+  );
 
   return (
     <ChartContainer className="w-67">
       <ChartHeader title="내 월간 소비 비교">
         <DropDown
-          selected={selectedId}
-          options={options}
+          selected={selectedCurrency}
+          options={CURRENCY_OPTIONS}
           size="xs"
           align="center"
-          onSelect={setSelectedId}
+          onSelect={setSelectedCurrency}
           itemWidth="w-20"
         />
       </ChartHeader>
-      <ChartContent className="h-56.5 flex-col px-4 py-5">
-        <ComparisonChartView selectedId={selectedId} />
+      <ChartContent
+        className="h-56.5 flex-col px-4 py-5"
+        skeleton={<ComparisonChartSkeleton />}
+        isLoading={isLoading}
+      >
+        <ComparisonChartView selectedId={selectedCurrency} />
       </ChartContent>
     </ChartContainer>
   );
