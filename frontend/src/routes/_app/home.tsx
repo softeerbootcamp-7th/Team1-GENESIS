55eda0874ec3893c64c332f96e7cd6c019865a02
1jiwoo27 <chloey1227@gmail.com>
2026-02-09 16:25:48 +0900

    chore: test 용 코드 삭제

--- frontend/src/routes/_app/home.tsx
+++ frontend/src/routes/_app/home.tsx
@@ -1,16 +1,11 @@
 import { createFileRoute } from '@tanstack/react-router';
 
-import ComparisonChart from '@/components/common/chart/comparison/ComparisonChart';
+import Homepage from '@/pages/Homepage';
 
 export const Route = createFileRoute('/_app/home')({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  return (
-    <div className="flex gap-3 p-5">
-      <ComparisonChart />
-      <ComparisonChart />
-    </div>
-  );
+  return <Homepage />;
 }
