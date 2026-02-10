8e42de71db235f2de3c219fc82b25abcc8fe3dd1
Mingyu <minngyuseong@gmail.com>
2026-02-10 17:45:31 +0900

    feat: DateFilter 구현 - TextInput과 달리 직접 입력하지 않고 Calendar 컴포넌트에서 클릭으로 입력받아야하기 때문에 CalendarInput 컴포넌트 추가

---
+++ frontend/src/components/common/CalendarInput.tsx
@@ -0,0 +1,53 @@
+import clsx from 'clsx';
+
+import { Icons } from '@/assets';
+
+interface CalendarInputProps {
+  title: string;
+  value: string;
+  onChange: (value: string) => void;
+  onClick: () => void;
+  onClear: () => void;
+}
+
+const CalendarInput = ({
+  title,
+  value,
+  onChange,
+  onClick,
+  onClear,
+}: CalendarInputProps) => {
+  const showClearButton = value.length > 0;
+
+  return (
+    <div className="flex flex-col gap-2 text-left">
+      <p className="label1-normal-bold text-label-neutral">{title}</p>
+      <div className="relative" onClick={onClick}>
+        {/* 너무 길어서 개행하기 위해 clsx 사용 */}
+        <input
+          className={clsx(
+            'bg-background-normal rounded-modal-12 px-4 py-3.25 pr-20',
+            'placeholder:body1-normal-regular placeholder:text-label-assistive',
+            'body2-normal-regular text-label-normal',
+            'border-line-normal-neutral border border-solid',
+            'focus:border-primary-normal/43 focus:border-2',
+          )}
+          placeholder="날짜를 입력해주세요."
+          value={value}
+          onChange={(e) => onChange(e.target.value)}
+        />
+
+        {showClearButton && (
+          <Icons.CloseCircle
+            className="text-label-neutral absolute top-1/2 right-11 size-5.5 -translate-y-1/2 cursor-pointer"
+            onMouseDown={(e) => e.preventDefault()}
+            onClick={onClear}
+          />
+        )}
+        <Icons.Calendar className="text-label-alternative absolute top-1/2 right-3 size-6 -translate-y-1/2" />
+      </div>
+    </div>
+  );
+};
+
+export default CalendarInput;
---
+++ frontend/src/components/common/data-table/filters/DateFilter.tsx
@@ -0,0 +1,158 @@
+import { useState } from 'react';
+
+import Calendar, {
+  type DateRange,
+} from '@/components/common/calendar/Calendar';
+import {
+  Popover,
+  PopoverAnchor,
+  PopoverContent,
+  PopoverTrigger,
+} from '@/components/ui/popover';
+
+import { formatDateToString } from '@/lib/utils';
+
+import {
+  DATE_PRESETS,
+  type DatePresetId,
+  getDateRangeFromPreset,
+} from '../../calendar/date.utils';
+import CalendarInput from '../../CalendarInput';
+import Divider from '../../Divider';
+import Filter from '../../Filter';
+
+const DateFilter = () => {
+  const [dateRange, setDateRange] = useState<DateRange>({
+    startDate: null,
+    endDate: null,
+  });
+  const [isFilterOpen, setIsFilterOpen] = useState(false);
+  const [isCalendarOpen, setIsCalendarOpen] = useState(false);
+
+  const isActive = !!(dateRange.startDate || dateRange.endDate);
+
+  const handleDateChange = ({
+    start,
+    end,
+  }: {
+    start: Date | null;
+    end: Date | null;
+  }) => {
+    setDateRange({ startDate: start, endDate: end });
+  };
+
+  const handleInputChange = (value: string, type: 'start' | 'end') => {
+    const newDate = new Date(value);
+
+    handleDateChange({
+      start: type === 'start' ? newDate : dateRange.startDate,
+      end: type === 'end' ? newDate : dateRange.endDate,
+    });
+  };
+
+  const handlePresetClick = (id: DatePresetId) => {
+    const { startDate, endDate } = getDateRangeFromPreset(id);
+    handleDateChange({ start: startDate, end: endDate });
+  };
+
+  const handleReset = () => {
+    handleDateChange({ start: null, end: null });
+  };
+
+  const filterLabel = (() => {
+    if (!isActive) return '날짜';
+    const startStr = dateRange.startDate
+      ? formatDateToString(dateRange.startDate)
+      : '';
+    const endStr = dateRange.endDate
+      ? formatDateToString(dateRange.endDate)
+      : '';
+    return `날짜: ${startStr} - ${endStr}`;
+  })();
+
+  return (
+    <Popover open={isFilterOpen} onOpenChange={setIsFilterOpen}>
+      <PopoverTrigger asChild>
+        <Filter
+          size="md"
+          isOpen={isFilterOpen}
+          active={isActive}
+          onReset={handleReset}
+        >
+          {filterLabel}
+        </Filter>
+      </PopoverTrigger>
+
+      <PopoverContent align="start" sideOffset={12} asChild>
+        <div className="bg-background-normal shadow-semantic-subtle rounded-modal-12 flex p-4">
+          {/* 프리셋 영역 */}
+          <div className="flex min-w-25 flex-col">
+            <p className="headline2-bold text-label-neutral mb-4 text-left">
+              기간 선택
+            </p>
+            {DATE_PRESETS.map((preset) => (
+              <button
+                key={preset.id}
+                onClick={() => handlePresetClick(preset.id)}
+                className="label1-normal-medium text-label-normal w-full cursor-pointer rounded-sm p-2 text-left"
+              >
+                {preset.label}
+              </button>
+            ))}
+          </div>
+
+          <Divider style="vertical" className="mx-6" />
+
+          {/* 달력 및 인풋 영역 */}
+          <Popover open={isCalendarOpen} onOpenChange={setIsCalendarOpen}>
+            <PopoverAnchor asChild>
+              <div className="flex flex-col justify-between">
+                <CalendarInput
+                  title="시작일"
+                  value={
+                    dateRange.startDate
+                      ? formatDateToString(dateRange.startDate)
+                      : ''
+                  }
+                  onClick={() => setIsCalendarOpen(true)}
+                  onChange={(val) => handleInputChange(val, 'start')}
+                  onClear={() =>
+                    handleDateChange({ start: null, end: dateRange.endDate })
+                  }
+                />
+                <CalendarInput
+                  title="종료일"
+                  value={
+                    dateRange.endDate
+                      ? formatDateToString(dateRange.endDate)
+                      : ''
+                  }
+                  onClick={() => setIsCalendarOpen(true)}
+                  onChange={(val) => handleInputChange(val, 'end')}
+                  onClear={() =>
+                    handleDateChange({ start: dateRange.startDate, end: null })
+                  }
+                />
+              </div>
+            </PopoverAnchor>
+
+            <PopoverContent
+              align="start"
+              side="right"
+              sideOffset={20}
+              alignOffset={-20}
+            >
+              <Calendar
+                startDate={dateRange.startDate}
+                endDate={dateRange.endDate}
+                onChange={handleDateChange}
+              />
+            </PopoverContent>
+          </Popover>
+        </div>
+      </PopoverContent>
+    </Popover>
+  );
+};
+
+export default DateFilter;
