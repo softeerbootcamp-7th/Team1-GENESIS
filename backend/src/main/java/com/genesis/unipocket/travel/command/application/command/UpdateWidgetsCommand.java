6dff0b4febcc4a0126dc487269822dd597dff4e0
김현창 <hyunchang52@gmail.com>
2026-02-10 10:58:08 +0900

    [BE] [Travel] 여행 도메인 리팩터링 (#303)
    * refactor(travel): presentation 계층 리팩토링
    - Command & Query 분리
    - DTO 분리
    - Swagger 주석 추가
    
    * refactor(travel): facade 계층 리팩토링
    - Command & Query 분리
    - DTO 분리
    - Command & Query 공통요소를 travel.common으로 분리
    
    * refactor(travel): application 계층 리팩토링
    - Command & Query 분리
    - DTO 분리
    
    * refactor(travel): application 계층 리팩토링
    - Command & Query 분리
    - DTO 분리
    - Command와 Query의 repository 분리
    
    * refactor(travel): travel 도메인 테스트 삭제
    
    * chore: Apply Spotless
    
    * refactor(travel): Repository 명 수정
    - JpaRepository -> CommandRepository로 이름 수정
    
    (cherry picked from commit f6938b16c25b88d7317f854f4c542bfa39c808dc)

---
+++ backend/src/main/java/com/genesis/unipocket/travel/command/application/command/UpdateWidgetsCommand.java
@@ -0,0 +1,15 @@
+package com.genesis.unipocket.travel.command.application.command;
+
+import com.genesis.unipocket.travel.command.presentation.request.WidgetRequest;
+import com.genesis.unipocket.travel.common.enums.WidgetType;
+import java.util.List;
+
+public record UpdateWidgetsCommand(Long travelId, List<Widget> widgets) {
+
+	public record Widget(WidgetType type, Integer order) {}
+
+	public static UpdateWidgetsCommand of(Long travelId, List<WidgetRequest> requests) {
+		List<Widget> widgets = requests.stream().map(r -> new Widget(r.type(), r.order())).toList();
+		return new UpdateWidgetsCommand(travelId, widgets);
+	}
+}
