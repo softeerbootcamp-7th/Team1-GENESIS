34b8766d8869903e0026668fa23d02094cbee3ba
Mingyu Seong <minngyuseong@gmail.com>
2026-02-11 21:36:19 +0900

    fix: proxy 재설정 - proxy는 로컬 개발환경에서만 작동하기 떄문에, 환경에 영향 받는 env 사용 없이 직접 target에 api서버 주소를 주입 - API_BASE_URL는 개발환경에 따라 달라져야하기에 env 사용

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,10 @@
 import tailwindcss from '@tailwindcss/vite';
 import { tanstackRouter } from '@tanstack/router-plugin/vite';
 import react from '@vitejs/plugin-react-swc';
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import svgr from 'vite-plugin-svgr';
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
-
+export default defineConfig(() => {
   return {
     base: '/',
     plugins: [
@@ -29,7 +27,7 @@ export default defineConfig(({ mode }) => {
     server: {
       proxy: {
         '/api': {
-          target: env.VITE_API_BASE_URL,
+          target: 'https://api.unipocket.co.kr',
           changeOrigin: true,
         },
       },
