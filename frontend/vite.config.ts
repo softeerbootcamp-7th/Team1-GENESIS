1767acf81333dacf20424ddd23f5d44884b01580
Mingyu <minngyuseong@gmail.com>
2026-02-12 00:13:04 +0900

    chore: vite 설정파일에 환경변수 사용하기

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,11 @@
 import tailwindcss from '@tailwindcss/vite';
 import { tanstackRouter } from '@tanstack/router-plugin/vite';
 import react from '@vitejs/plugin-react-swc';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import svgr from 'vite-plugin-svgr';
 
-export default defineConfig(() => {
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
   return {
     base: '/',
     plugins: [
@@ -27,7 +28,7 @@ export default defineConfig(() => {
     server: {
       proxy: {
         '/api': {
-          target: 'https://api.unipocket.co.kr',
+          target: env.VITE_API_PROXY_TARGET,
           changeOrigin: true,
         },
       },
