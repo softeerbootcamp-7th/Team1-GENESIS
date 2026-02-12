4cadb791e382f88688a909ffb2496d7fd47baeb2
BE_김동균 <160785874+AnarchyDeve@users.noreply.github.com>
2026-02-12 10:50:15 +0900

    chore: spotlessApply 적용

--- backend/src/test/java/com/genesis/unipocket/auth/command/presentation/AuthCommandControllerTest.java
+++ backend/src/test/java/com/genesis/unipocket/auth/command/presentation/AuthCommandControllerTest.java
@@ -22,8 +22,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(AuthCommandController.class)
