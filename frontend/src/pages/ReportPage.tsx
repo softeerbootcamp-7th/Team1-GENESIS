b5aa568d5ae5e3d816c75ffe7e12b36af6ddb0ef
1jiwoo27 <chloey1227@gmail.com>
2026-02-12 10:25:28 +0900

    chore: store 불러오는 불필요한 코드 삭제

--- frontend/src/pages/ReportPage.tsx
+++ frontend/src/pages/ReportPage.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 import ReportCategory from '@/components/report-page/category/ReportCategory';
 import mockData from '@/components/report-page/mock';
@@ -8,22 +8,10 @@ import ReportProvider from '@/components/report-page/ReportProvider';
 
 import { type CurrencyType } from '@/types/currency';
 
-import { mockData as accountBookMock } from '@/stores/mock';
-import { useAccountBookStore } from '@/stores/useAccountBookStore';
-
 const ReportPage = () => {
-  const accountBook = useAccountBookStore((state) => state.accountBook);
   const categoryData = mockData.compareByCategory;
   const myselfData = mockData.compareWithLastMonth;
   const [currencyType, setCurrencyType] = useState<CurrencyType>('LOCAL'); // @TODO: 드롭다운 추가 예정
-
-  // 추후 삭제될 예정
-  useEffect(() => {
-    if (!accountBook) {
-      console.log('Setting mock account book data');
-      useAccountBookStore.getState().setAccountBook(accountBookMock);
-    }
-  }, [accountBook]);
 
   return (
     <div className="flex gap-3.5 p-3">
