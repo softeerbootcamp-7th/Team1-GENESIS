8ce96574113cbfbb547e3d945b5b22c33455c0a9
blu3fishez <kcw2205@naver.com>
2026-02-13 12:02:24 +0900

    [BE] [Auth] 사용되지 않는 JwtProvider 내 헬퍼 메서드삭제
    - feat: 코드베이스 대폭변경으로 인해 사용되지 않는 메서드 삭제

--- backend/src/main/java/com/genesis/unipocket/auth/command/application/JwtProvider.java
+++ backend/src/main/java/com/genesis/unipocket/auth/command/application/JwtProvider.java
@@ -167,12 +167,4 @@ public class JwtProvider {
 			return 0;
 		}
 	}
-
-	public int getAccessTokenExpirationAsSeconds() {
-		return (int) (accessTokenExpiration / 1000);
-	}
-
-	public int getRefreshTokenExpirationAsSeconds() {
-		return (int) (refreshTokenExpiration / 1000);
-	}
 }
